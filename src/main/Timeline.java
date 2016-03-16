98108a35b98ac6772ef930574e249f2e684746fc
CableErector <cableerector@gmail.com>
2016-03-16 21:15:12 +0100

    Added early backbone

--- src/main/Timeline.java
+++ src/main/Timeline.java
@@ -1,5 +1,32 @@
 package main;
+import java.util.*;
 
 public class Timeline {
-
+    private int t;
+    private List<Note> timeline;
+    
+    public Timeline()
+    {
+        timeline = new ArrayList<>();
+        //timeline.add(new Note(1,1,1));
+        t = 0;
+    }
+    
+    public Note NextNote()
+    {
+        if(timeline.isEmpty())
+            return null;
+        else
+            return timeline.get(t++);
+    }
+    
+    public boolean AppendNote(Note e)
+    {
+        return timeline.add(e);
+    }
+    
+    public boolean AppendNote(Collection<Note> c)
+    {
+        return timeline.addAll(c);
+    }
 }

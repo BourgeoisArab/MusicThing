691ab71aad9bb83fe01444bfe1b79e055d537d0c
jakoubeck503 <jakoubeck@maincube.net>
2016-03-16 16:01:51 +0100

    .

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,9 @@
 package main;
-import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// Hello from netbeans
+		// TODO: everything
 	}
 
 }
--- src/main/Timeline.java
+++ src/main/Timeline.java
@@ -1,29 +1,29 @@
 package main;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class Timeline {
-    private int t;
-    private List<Note> timeline;
-    
-    public Timeline()
-    {
-        timeline = new ArrayList<>();
-        //timeline.add(new Note(1,1,1));
-        t = 0;
-    }
-    
-    public Note NextNote()
-    {
-        return ( timeline.isEmpty() ? null : timeline.get(t++) );
-    }
-    
-    public boolean AppendNote(Note e)
-    {
-        return timeline.add(e);
-    }
-    
-    public boolean AppendNote(Collection<Note> c)
-    {
-        return timeline.addAll(c);
-    }
+
+	private int t;
+	private List<Note> timeline;
+
+	public Timeline() {
+		timeline = new ArrayList<Note>();
+		// timeline.add(new Note(1,1,1));
+		t = 0;
+	}
+
+	public Note NextNote() {
+		return timeline.isEmpty() ? null : timeline.get(t++);
+	}
+
+	public boolean AppendNote(Note e) {
+		return timeline.add(e);
+	}
+
+	public boolean AppendNote(Collection<Note> c) {
+		return timeline.addAll(c);
+	}
 }

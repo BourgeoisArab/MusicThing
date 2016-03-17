cba9610600a1cc2e381e4bc4d04899f7dbb3b6e5
CableErector <cableerector@gmail.com>
2016-03-17 12:16:16 +0100

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,11 +5,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		// Hello from netbeans
-                Timeline t = new Timeline();
-                List<Note> l = new ArrayList<>();
-                for(int i = 0;i < 5;i++)
-                    l.add(new Note(1,1,1));
-                t.AppendNote(l);
 	}
 
 }

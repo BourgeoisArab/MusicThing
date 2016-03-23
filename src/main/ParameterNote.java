d34f2dd17e09ebafc5808007330bdd1faa07aa2c
jakoubeck503 <jakoubeck@maincube.net>
2016-03-23 14:11:17 +0100

    Added abstraction

--- src/main/ParameterNote.java
+++ src/main/ParameterNote.java
@@ -1,33 +1,45 @@
 package main;
 
-public class ParameterNote extends Note {
-    private int[] timeSignature;
-    private Key key;
-    
-    public ParameterNote(int p, float f, float d,int[] ts,Key k)
-    {
-        super(p,f,d);
-        key = k;
-        timeSignature = ts;
-    }
-    
-    public int[] GetTimeSig() 
-    { 
-        return timeSignature; 
-    }
-    
-    public void SetTimeSig(int[] ts) 
-    { 
-        timeSignature = ts; 
-    }
-    
-    public Key GetKey() 
-    { 
-        return key; 
-    }
-    
-    public void SetKey(Key k) 
-    { 
-        key = k; 
-    }
+public abstract class ParameterNote extends Note {
+
+	public ParameterNote(int pitch, float frequency, float duration) {
+		super(pitch, frequency, duration);
+	}
+
+	public static class TimeChange extends ParameterNote {
+
+		private int[] timeSignature;
+
+		public TimeChange(int p, float f, float d, int[] ts) {
+			super(p, f, d);
+			timeSignature = ts;
+		}
+
+		public int[] GetTimeSig() {
+			return timeSignature;
+		}
+
+		public void SetTimeSig(int[] ts) {
+			timeSignature = ts;
+		}
+	}
+
+	public static class KeyChange extends ParameterNote {
+
+		private Key key;
+
+		public KeyChange(int pitch, float frequency, float duration, Key key) {
+			super(pitch, frequency, duration);
+			this.key = key;
+		}
+
+		public Key GetKey() {
+			return key;
+		}
+
+		public void SetKey(Key k) {
+			key = k;
+		}
+	}
+
 }

0659c166f4c35454035f94f87bdde2e658ac4506
CableErector <cableerector@gmail.com>
2016-03-25 21:13:53 +0100

    nicer look

--- src/core/Dynamics.java
+++ src/core/Dynamics.java
@@ -2,7 +2,14 @@ package core;
 
 public enum Dynamics {
 
-    ppp(20), pp(31), p(42), mp(53), mf(64), f(80), ff(96), fff(112);
+    ppp(20), 
+    pp(31),
+    p(42),
+    mp(53),
+    mf(64),
+    f(80),
+    ff(96),
+    fff(112);
 
     public final byte value;
 

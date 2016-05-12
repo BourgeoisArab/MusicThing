f5cb87079f51f56a36c8d57f77d7995a43cc1af9
jakoubeck503 <jakoubeck@maincube.net>
2016-05-12 16:15:12 +0200

    Added articulations and cadence classes

---
+++ src/core/Cadence.java
@@ -0,0 +1,13 @@
+package core;
+
+public enum Cadence {
+
+	perfect(4, 0), plagal(3, 0), imperfect(5), interrupted(5, 6);
+
+	public final int[] chords;
+
+	Cadence(int... chords) {
+		this.chords = chords;
+	}
+
+}

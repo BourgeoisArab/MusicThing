d7b3eeef55ed1d6dea0832b1007cca23d97a4ec6
CableErector <CableErector@gmail.com>
2016-06-19 12:12:53 +0200

    duration->total_duration Added: absoluteRatio() and compareTo(Rhythm r) for comparing ratios of different rhythms

--- src/core/Rhythm.java
+++ src/core/Rhythm.java
@@ -4,38 +4,50 @@ public class Rhythm {
     // Rhythm's notes ratio based on duration
     public int[] ratio;
     // Note duration 1/4 - crotchet
-    public int duration;
+    public int total_duration;
     
-    // 2 Eighth notes - d = 1/4, r = 1/2, 1/2
+    // 2 Eighth notes - td = 1/4, r = 1/2, 1/2
     /**
      * 
-     * @param r - rhythmic ratios, r sum <= 1. Negative ratios signify rests
-     * @param d - rhythm duration relative to that of a semi-breve
+     * @param r - rhythmic ratios, r sum <= 1
+     * @param td - rhythm's total duration relative to that of a semi-breve (1)
      */
-    public Rhythm(int [] r,int d)
+    public Rhythm(int [] r,int td)
     {
         ratio = r;
-        duration = d;
+        total_duration = td;
+    }
+    public boolean compareTo(Rhythm r) 
+    {
+       return this.ratio == r.ratio;
+    }
+        public int[] abosoluteRatio()
+    {
+        int[] absoluteRatio = new int[ratio.length];
+        for(int i = 0;i < ratio.length-1;i++)
+            absoluteRatio[i] = ratio[i] * total_duration;
+        return absoluteRatio;
+    }
+    public int absoluteDuration()
+    {
+        int absoluteDuration = 0;
+        for(int i = 0;i < ratio.length-1;i++)
+            absoluteDuration += ratio[i];
+        return absoluteDuration * total_duration;
     }
     
-    public static Rhythm[] Split(int step) // Splits d and ratios symmetrically by step(ratio)
+    
+    public static Rhythm[] split(int step) // Splits d and ratios symmetrically by step(ratio)
     {
          return new Rhythm[]{};
     } 
-    public static Rhythm Combine(Rhythm c) // Combined ratios, increased d
+    public static Rhythm combine(Rhythm c) // Combined ratios, increased d
     {
         return new Rhythm(new int[1],0);     
     }
-    public static Rhythm Merge(Rhythm c) // merged rations, same d
+    public static Rhythm merge(Rhythm c) // merged rations, same d
     {
         return new Rhythm(new int[1],0);
-    }
-    public int[] AbosluteDuration()
-    {
-        int[] absoluteDuration = new int[ratio.length];
-        for(int i = 0;i < ratio.length-1;i++)
-            absoluteDuration[i] = ratio[i] * duration;
-        return absoluteDuration;
     }
 }
 
